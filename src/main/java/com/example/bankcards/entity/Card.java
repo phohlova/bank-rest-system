8f6bb7ea428da48d0d5c28da688d97c6ef8016cf
phohlova <hohlova.pv@edu.spbstu.ru>
2026-05-03 14:30:24 +0300

    feat: add Card entity

---
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -0,0 +1,40 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cards")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "card_number", unique = true, nullable = false, length = 16)
+    private String cardNumber;
+
+    @Column(name = "card_holder", nullable = false)
+    private String cardHolder;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDate expiryDate;
+
+    @Column(name = "cvv", nullable = false, length = 3)
+    private String cvv;
+
+    @Column(name = "balance", nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private CardStatus status;
+}
